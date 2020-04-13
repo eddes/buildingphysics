7244b96e3bf258b3a708bba6ded113f56eedf4df
eddes <eddes@users.noreply.github.com>
2020-04-13 18:38:55 +0200

    Update PID_heater.py

--- chapter_2/3_HVAC_control/PID_heater.py
+++ chapter_2/3_HVAC_control/PID_heater.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #quelques couleurs
 rouge_A='#C60C2E'
 vert1_A='#005157'
@@ -13,49 +12,54 @@ from scipy.optimize import fsolve
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import sys
+
+# function for Crank-Nicolson's scheme
+def fc_CN(Tp,T, dt, m_Cp, P_heater,US_ext, T_amb,T_ext):
+	return  -Tp +T +0.5*(dt/m_Cp*(P_heater-US_ext*(T-T_ext))) + 0.5*(dt/m_Cp*(P_heater-US_ext*(Tp-T_ext)))
 
 
-def fc_valve_curve(caracteristique, valve_pos):
-	if caracteristique == "linear":
+def fc_valve_curve(characteristic, valve_pos):
+	if characteristic == "linear":
 		Kv_ratio =  valve_pos
-	elif caracteristique == "equal_pct":
+	elif characteristic == "equal_pct":
 		Kv_ratio = np.exp(3.5 * (valve_pos - 1))
-	elif caracteristique == "quadratic":
+	elif characteristic == "quadratic":
 		Kv_ratio =  valve_pos**2
-	elif caracteristique == "square_root":
+	elif characteristic == "square_root":
 		Kv_ratio = np.power(valve_pos,0.5)
 	return Kv_ratio 
 
-caracteristique="linear"
-#caracteristique="equal_pct"
-#caracteristique="quadratic"
+#characteristic="linear"
+characteristic="equal_pct"
+#characteristic="quadratic"
 
 
 # Geometrical data
-m_Cp = 1.6 * 10 * 2000 #kJ/K = Cp*V*rho
-S_ext = 50 # m2 envelope's surface to the outdoor
-U_mur = 1 # W/m2/K
-US_ext = U_mur * S_ext
-Cp = 4182 # kJ/kg/K pour l#eau
+m_Cp = 1006 * 5*5*2.5* 1 #kJ/K = Cp*V*rho
+S_ext = 50 #m2 envelope surface to outdoors
+U_wall = 1 #W/m2/K
+US_ext = U_wall*S_ext
+Cp = 4182 #kJ/kg/K water heat capacity
 
 # 
-Pmax = 1500 # heat power 
-US_radiateur = Pmax / np.power(50,1.3) #DTLM reference = 50K
-qm = Pmax / (Cp * 20) # flowrate for 20K temperature difference
+Pmax = 3000 # maximum heater power 
+US_heater = Pmax / np.power(50,1.3) #DTLM nominal = 50K
+qm = Pmax / (Cp * 20) # flowrate
 
-Tmax = 60 #Tmax inlet water
+Tmax = 60 #Tmax eau
 T_in = Tmax #
-T_out = T_in-10 # initialize T_out with 10K temperature drop
+T_out = T_in-5 #initialisation of the dT
 T_set=20
-T_init=10
+T_init=16
 T_amb = T_init
 T_ext=5 #outdoor
 
 # PID parameters
-prop_band = 10# K
-Kp = 1 / prop_band  # passage au gain
-Tn = 30
-Td = 0
+prop_band =5# K
+Kp = 1/prop_band  # passage au gain
+Tn = 3000
+Td = 50
 auth=0.5 #valve authority
 
 # initial values
@@ -64,7 +68,7 @@ sim_time= 3600 # simulation duration s
 t=0
 dt=10 # [s] time step /sampling rate
 
-# initialize PID control values
+#Remise à zero des paramètres de calcul de la regulation
 sum_error = 0
 delta_error = 0
 valve_position = 0
@@ -77,46 +81,64 @@ v_pos=[]
 
 def fc_solve_T_out(T_out,T_in,T_amb):
 	DTLM=(T_in-T_out)/math.log((T_in-T_amb)/(T_out-T_amb))
-	return  qm*Cp*(T_in-T_out)-US_radiateur*np.power(DTLM,1.3)
+	return  qm*Cp*(T_in-T_out)-US_heater*np.power(DTLM,1.3)
 
-P=0.5*Pmax
+# initializations
+P_heater=0.5*Pmax 
 valve_position=1
 deltaT_previous=0
+delta_error=0
 sum_error=0
 QA=0
+# time loop
 while t <= sim_time:
 	# if not heating required
-	if T_out==T_amb:
-#		print("froid", T_in,T_out,T_amb,QA)
-		P_radiateur=0
-		# Euler explicit
-		T_amb = T_amb + dt / m_Cp * (P_radiateur - US_ext * (T_amb - T_ext))
+	if T_amb>T_set:
+		QA=0
+		T_out=T_amb  #aesthetical fill
+		T_in = T_out #aesthetical fill
+		P_heater=0
+		# Crank Nicolson
+		T_amb=fsolve(fc_CN, T_amb, args=(T_amb, dt, m_Cp, P_heater,US_ext, T_amb,T_ext))
 		# I action
 		sum_error = dt / Tn * (T_set - T_amb) + sum_error
 		# D action
 		delta_error = (T_set - T_amb) - deltaT_previous
 		# valve pos
 		valve_position = Kp * ((T_set - T_amb) + sum_error + Td * delta_error / dt)
+		v_share = Kp * (abs(T_set - T_amb) + abs(sum_error) + Td *abs( delta_error )/ dt)
+		#Control for valve opening
+		if valve_position < 0:
+			valve_position = 0
+			QA=0
+#			T_out=T_amb
+		elif valve_position > 1:
+			valve_position = 1
+			QA=1
+		else:
+			Kv_ratio=fc_valve_curve(characteristic, valve_position)
+			QA = np.power( (auth * np.power(Kv_ratio, -2) + 1 - auth),-0.5)
 	# heating required
 	else:
-#		print("chaud", T_in,T_out,T_amb,QA)
-		# avoid div by zero
-		if (T_in-T_amb)==(T_out-T_amb):
-			P_radiateur=qm*Cp*(T_in-T_out)
-		else:
-			# solve for T_out with initial guess T_out ~ T_in-P/(qm*Cp)
-			T_out= fsolve(fc_solve_T_out, T_in-P/(qm*Cp), args=(T_in,T_amb))
-			# DTLM
-			DTLM=(T_in-T_out)/math.log((T_in-T_amb)/(T_out-T_amb))
-			P_radiateur=US_radiateur * np.power(DTLM,1.3)
-		# Euler explicit
-		T_amb = T_amb + dt / m_Cp * (P_radiateur - US_ext * (T_amb - T_ext))
+		T_out= fsolve(fc_solve_T_out, T_in-P_heater/(qm*Cp), args=(T_in,T_amb))
+		# DTLM
+		DTLM=(T_in-T_out)/math.log((T_in-T_amb)/(T_out-T_amb))
+		# avoid div by zero and NaN when heating required after non heating period
+		if np.isnan(DTLM):
+			T_out=max((T_in-0.1),(T_amb-0.1)) # well, you gotta start somewhere
+			DTLM=(T_in-T_out)/math.log((T_in-T_amb)/(T_out-T_amb)) # avoid math error
+		P_heater=US_heater * np.power(DTLM,1.3)
+
+		# Crank Nicolson
+		T_amb=fsolve(fc_CN, T_amb, args=(T_amb, dt, m_Cp, P_heater,US_ext, T_amb,T_ext))
 		# I action
 		sum_error = dt / Tn * (T_set - T_amb) + sum_error
 		# D action
 		delta_error = (T_set - T_amb) - deltaT_previous
 		# valve pos
 		valve_position = Kp * ((T_set - T_amb) + sum_error + Td * delta_error / dt)
+		v_share = Kp * (abs(T_set - T_amb) + abs(sum_error) + Td *abs( delta_error )/ dt)
+		
 		#Control for valve opening
 		if valve_position < 0:
 			valve_position = 0
@@ -126,17 +148,19 @@ while t <= sim_time:
 			valve_position = 1
 			QA=1
 		else:
-			Kv_ratio=fc_valve_curve(caracteristique, valve_position)
+			Kv_ratio=fc_valve_curve(characteristic, valve_position)
 			QA = np.power( (auth * np.power(Kv_ratio, -2) + 1 - auth),-0.5)
-		# compute the 
-		T_in = (QA * Tmax + (1-QA) * T_out) # mixing temperature
-		
+	
+	# compute the inlet temperature
+	T_in = (QA * Tmax + (1-QA) * T_out) # mixing temperature
+
+#		sys.exit(0)
 	v_pos.append(valve_position)
 	# let#s see and plot who does what at each time step
 	if valve_position > 0:
-		pcent_P.append(abs (T_set - T_amb) * Kp / valve_position)
-		pcent_I.append( sum_error + Kp*abs (T_set - T_amb)* (dt / Tn) / valve_position)
-		pcent_D.append(Kp * (Td * delta_error / dt) / valve_position)
+		pcent_P.append(abs (T_set - T_amb) * Kp / v_share)
+		pcent_I.append(Kp*(abs(sum_error))/ v_share)
+		pcent_D.append(Kp * (Td *abs(delta_error) / dt) / v_share)
 	else:
 		pcent_P.append(0)
 		pcent_I.append(0)
@@ -148,10 +172,11 @@ while t <= sim_time:
 	T_ambl.append(T_amb)
 	T_inl.append(T_in)
 	T_outl.append(T_out)
-	heater.append(US_radiateur * np.power(DTLM,1.3))
+	heater.append(P_heater) # US_heater * np.power(DTLM,1.3)
 	losses.append(US_ext * (T_amb - T_ext))
 
 plt.subplot(121)
+
 plt.xlabel("Time [s]")
 plt.ylabel("Temperature [°C]")
 plt.plot(time, T_ambl,color=coule[3],linestyle="-",alpha=1,marker='',label='room')
@@ -163,11 +188,12 @@ plt.legend()
 plt.subplot(122)
 plt.xlabel("Time [s]")
 plt.ylabel("Share of P, I and D in the valve opening [-]")
-plt.plot(time, pcent_P,color=coule[0],linestyle="-",alpha=1,marker='',label="P")
-plt.plot(time, pcent_I,color=coule[1],linestyle="-",alpha=1,marker='',label="I")
+plt.plot(time[1:], v_pos[1:],color=coule[-1],linestyle="--",alpha=1,marker='',label="valve position")
+plt.plot(time[1:], pcent_P[1:],color=coule[0],linestyle="-",alpha=1,marker='',label="P")
+plt.plot(time[1:], pcent_I[1:],color=coule[1],linestyle="-",alpha=1,marker='',label="I")
 if Td != 0:
-	plt.plot(time, pcent_D,color=coule[2],linestyle="-",alpha=1,marker='',label="D")
-
+	plt.plot(time[1:], pcent_D[1:],color=coule[2],linestyle="-",alpha=1,marker='',label="D")
 
 plt.legend()
 plt.tight_layout()
+plt.savefig("./PID_heater.pdf",dpi=200,bbox_inches='tight')
