a018622b2176f0f65a7e4fc4ea074e8ef1f1ba5c
eddes <edouard.walther@gmail.com>
2020-05-04 22:40:15 +0200

    Add comments to the code

--- chapter_1/1_explicit_euler_vector.py
+++ chapter_1/1_explicit_euler_vector.py
@@ -1,24 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# number of nodes
 n=100
+# prepare the T and T+ arrays
 T_plus=np.zeros(n)
 T=np.zeros(n)
-
-t=0
+# time
 sim_time=600
 dt=1
 
+#geometry and thermal props
 L=0.1 # m
+dx=L/n #
 alpha=1e-7 #m2/s
-dx=L/n # 
+Fo=alpha*dt/dx**2 # Fourier
 
-Fo=alpha*dt/dx**2
-#Fo=np.ones(n)*0.25 # >> local Fo for PCMs
-# 
+# stability check
 if Fo>0.5:
 	print("stability issue")
-# time 
+# time
+t=0
 while t < sim_time:
 	# boundary conditions
 	T[0]=0
@@ -26,11 +28,12 @@ while t < sim_time:
 	# inside the domain 
 	for i in range(1,n-1):
 		T_plus[i]=T[i]*(1-2*Fo)+Fo*(T[i+1]+T[i-1])
-	T=T_plus # replace 
+	T=T_plus # replace T by T+
 	t+=dt
-	
+# plotting
 x_pos=np.arange(0,L,dx)
 plt.xlabel("x position [m]")
 plt.ylabel("Temperature [°C]")
 plt.plot(x_pos, T_plus, 'o-', alpha=0.65)
-plt.savefig("./FDM_t"+str(sim_time)+".png",dpi=200)
+plt.show()
+# plt.savefig("./FDM_t"+str(sim_time)+".pdf") # optionally save
