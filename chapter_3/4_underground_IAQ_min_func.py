35a795420cae05252d9e47d1cde4129231fd6b1a
eddes <edouard.walther@gmail.com>
2020-05-04 23:21:26 +0200

    Added comments to the code

--- chapter_3/4_underground_IAQ_min_func.py
+++ chapter_3/4_underground_IAQ_min_func.py
@@ -15,22 +15,20 @@ def fc_crank_nicolson(Cp, C, dt, alpha, beta, tau, delta, N, Ce):
     term_Cp = dt * (alpha * N ** 2 + (beta * N + tau) * (Ce - Cp) - delta * Cp)
     return -Cp + C + 0.5 * term_C + 0.5 * term_Cp
 
-
+# a boolean in order to have only one function
 minimisation = False
-
 
 def min_func(x):
     alpha, beta, delta, tau = x
-    dt = 0.25  # 1/h
-    time_meas = np.loadtxt("time")
-    sim_time = max(time_meas)
-    traffic = np.loadtxt("traffic")
-    PM10 = np.loadtxt("PM10")
-    PM10 = PM10 * 100
-    Ce = 15
-    Cinit = PM10[0]
-    Ce = 15
-    C = Cinit
+    dt = 0.25  # 1/h time step
+    #measured values
+    time_meas = np.loadtxt("time") # the time at which measurements were taken
+    sim_time = max(time_meas) # the simulation time corresponds to measurements
+    traffic = np.loadtxt("traffic") # train traffic (#/h)
+    PM10 = np.loadtxt("PM10") # normalised PM10
+    PM10 = PM10 * 100 # ... let's scale them to 100
+    Ce = 15 # outdoor PM10
+    C = PM10[0] # initialise with the measurement
     t, i = min(time_meas), 0
     time, concentration = [], []
     while t <= max(time_meas):
@@ -53,18 +51,18 @@ def min_func(x):
 
 
 minimisation = True
+# bounds for the parameters
 #           alpha      beta     delta      tau
 bnds = ((0.2, 0.5), (0.05, 0.2), (0.01, 10), (0.1, 1))
-k = 0.5
+k = 0.5 # starting point in the middle of all bounds
 x0 = [k * (bnds[0][0] + bnds[0][1]),
       k * (bnds[1][0] + bnds[1][1]),
       k * (bnds[2][0] + bnds[2][1]),
       k * (bnds[3][0] + bnds[3][1])]
-
-# sol = minimize(min_func, x0, bounds=bnds, method='L-BFGS-B', tol=1e-3)
-# sol = minimize(min_func, x0, bounds=bnds, method='SLSQP', tol=1e-3)
+#minimisation procedure
 sol = minimize(min_func, x0, bounds=bnds, method='TNC', tol=1e-3)
 
+# now let us plot the results
 alpha, beta, delta, tau = sol.x[0], sol.x[1], sol.x[2], sol.x[3]
 minimisation = False
 time, concentration, mean_error = min_func([alpha, beta, delta, tau])
@@ -78,7 +76,6 @@ plt.clf()
 plt.xlabel(r"Time [h]")
 plt.ylabel(r"Underground $PM_{10}$ [µg/m$^3$]")
 plt.plot(time, concentration, color=coule[-1], linestyle="--", alpha=0.75, marker='', label='model')
-plt.plot(time, PM10, color=coule[1], linestyle="", alpha=0.25, marker='o', markersize='2.5',
-         label='scaled measurements')
+plt.plot(time, PM10, color=coule[1], linestyle="", alpha=0.25, marker='o', markersize='2.5', label='scaled measurements')
 plt.legend()
-plt.savefig("./PM10_underground.pdf", dpi=200, bbox_inches='tight')
+plt.show()
