c0e0835ccd3bb6367f328a82e5848f9fe9ab3b76
eddes <edouard.walther@gmail.com>
2020-05-04 22:48:07 +0200

    Add comments to the code

--- chapter_3/1_single_class_coupled_IAQ.py
+++ chapter_3/1_single_class_coupled_IAQ.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#quelques couleurs
+# a few colors
 rouge_A='#C60C2E'
 vert1_A='#005157'
 vert2_A='#627D77'
@@ -21,37 +21,45 @@ def fc_IAQ_coupled(vec_CLp, vec_CL, tau,delta,dt,rho,S,V,Ce):
 	L_term = -Lp + L + 0.5*dt*( delta*V*C/S - rho*S*L/S ) + 0.5*dt*( delta*V*Cp/S - rho*S*Lp/S )
 	return [C_term,L_term]
 
-# enclosure
-L,l,h=5,5,3# dimensions
+# enclosure features
+# dimensions
+L,l,h=5,5,3 #m3
+# surface
 S=2*( L*l+l*h+ h*L) # m2
-V=L*l*h # m3 
+#volume
+V=L*l*h # m3
+# air change rate
 tau=0.1# vol/h
+# flow rate
 qv=V*tau#m3/h
-tau=qv/V
 
-Cinit=20
-Linit=100
-Ce_base=20
-delta=0.15#	1/h
-rho_base=0.5 # initial efficiency
+# definitions
+Cinit=20 # initial C [\mu g/m3]
+Linit=100 # initial L [\mu g/m2]
+Ce_base=20 # base outdoor concentratio n
+delta=0.15#	1/h deposition rate
+rho_base=0.5 # base resuspension rate
 
+# initialize
 C,L=Cinit,Linit
-concentration,deposition,time=[],[],[]
-rhoC,deltaC=[],[]
-rhop=[]
+concentration,deposition,time=[],[],[] # for storage and plotting
+rhoC,deltaC=[],[] # same
+rhop=[] # same same
 
-nb_period=2
-period=24
-dt=0.1 #h
+nb_period=2 # how many periods are to be simulated ?
+period=24 # for the periodic resuspension rate
+dt=0.1 # h (this time dt is in [hours])
 sim_time=nb_period*24 # hour
-t=0 # hour
-
+t=0 # initialize
+# time loop
 while t < sim_time:
-	Ce=Ce_base + 5*(np.cos(t*2*np.pi/period))
-	rho=rho_base*abs(np.sin(t*2*np.pi/period))
+	Ce=Ce_base + 5*(np.cos(t*2*np.pi/period)) # update outdoor concentration
+	rho=rho_base*abs(np.sin(t*2*np.pi/period)) # update resuspension rate
+	# compute C+ and L+
 	C_plus,L_plus=fsolve(fc_IAQ_coupled, [C,L], args=([C,L],tau,delta,dt,rho,S,V,Ce))
 	C,L=C_plus,L_plus
 	t+=dt
+	# store for plotting
 	concentration.append(C)
 	deposition.append(L)
 	time.append(t)
@@ -59,35 +67,31 @@ while t < sim_time:
 	rhoC.append(rho*S*L/V)
 	deltaC.append(delta*C)
 	
-	
-plt.subplot(121)
+# plotting
+plt.subplot(221)
 plt.xlabel("Time [h]")
 plt.ylabel(r"Concentration [µg/m$^3$]")
 plt.plot(time, concentration, '-',color=coule[0], alpha=0.65,label='C')
-#plt.plot(time, rhop, '--',color=coule[1], alpha=0.65,label='rho')
 plt.legend()
 
-plt.subplot(122)
+plt.subplot(222)
 plt.xlabel("Time [h]")
 plt.ylabel(r"Mass on surfaces [µg/m$^2$]")
 plt.plot(time, deposition, '-',color=coule[-1], alpha=0.65,label='L')
 plt.legend()
 
-plt.tight_layout()
-
-
-plt.clf()
-plt.subplot(121)
+plt.subplot(223)
 plt.xlabel("Time [h]")
 plt.ylabel(r"Resuspension rate [µg/m$^3$]")
 plt.plot(time, rhop, '-',color=coule[0], alpha=0.65,label=r'$\rho$')
 plt.legend()
 
-plt.subplot(122)
+plt.subplot(224)
 plt.xlabel("Time [h]")
-plt.ylabel(r"Transfer to air [µg/m$^{3}$] and surfaces [µg/m$^{2}$]")
+plt.ylabel(r"Transfer to air and surfaces [µg/m$^{3}/s$]")
 plt.plot(time, rhoC, '--',color=coule[3], alpha=0.65,label=r'$ \frac{\rho L S}{V}$')
 plt.plot(time, deltaC, '-',color=coule[2], alpha=0.65,label=r'$\delta C$')
 plt.legend()
 
 plt.tight_layout()
+plt.show()
