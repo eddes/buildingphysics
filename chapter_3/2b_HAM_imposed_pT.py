754a3810b549018876013e9c8f1601b9ddc9d218
eddes <eddes@users.noreply.github.com>
2020-12-13 01:05:23 +0100

    Update and rename 2b_heat_and_mass_transfer.py to 2b_HAM_imposed_pT.py

--- chapter_3/2b_HAM_imposed_pT.py
+++ chapter_3/2b_HAM_imposed_pT.py
@@ -22,27 +22,30 @@ start_time = time.time()
 # vapour pressure at saturation
 def fc_pvsat(T):
 	return np.power(10, 7.625*T/(241+T)+2.7877)
-
-# Cp depending on water content
-def fc_Cp_w(w_vec,rho,Cp):
-	Cp_vec=np.ones(len(w_vec))
-	Cpw=4182
-	return Cp*Cp_vec+w/rho*Cpw
-
+# vapour diffusivity depending on water content 
 def fc_deltav(w):
 	a=1.1*1e-7
 	b=-1.57*1e-10
 	return a+b*w
-
+# thermal conductivity depending on water content 
 def fc_lambda(w):
 	a=0.23
 	b=0.006
 	return a+b*w
+# sorption curve
 def fc_w_phi(phi):
 	a=700 #1000
 	b=250*1e-6 # 146*1e-6
 	c=2.9#1.59
-	return a/np.power( 1- np.log(phi/100)/b, 1/c)
+	# let's try to avoid numerical problems 
+	# with invalid log values...
+	try: 
+		res=a/np.power( 1- np.log(phi/100)/b, 1/c)
+	# ... hence the upper limit for w
+	except: 
+		print(phi)
+		res=600
+	return res
 
 # update local Fourier 
 def update_Fo(w_vec,k,rho,Cp,dt,dx):
@@ -73,41 +76,29 @@ def update_Cpm(w,phi,T):
 #	Equation solving
 # 
 def fc_coupled_HAM(vec_Tpvp,vec_Tpv,K,Fo,Fow,dt,rho,Cp,Cpm,Lv):
-	rcp=rho*Cp
 	# split array 
-	n=int(len(vec_Tpv)/2) # number of size classes
+	n=int(len(vec_Tpv)/2) # half index
 	T,pv   = vec_Tpv[0:n] ,vec_Tpv[n:]
 	Tp,pvp = vec_Tpvp[0:n],vec_Tpvp[n:]
-	# Crank-Nicolson scheme
-	T_term  = -Tp  +T   +0.5*(Fo*np.dot(K,T) + Lv/rcp*Fow*np.dot(K,pv) ) +0.5*(Fo*np.dot(K,Tp)+Lv/rcp*Fow*np.dot(K,pvp))
-	pv_term = -pvp + pv +0.5*(Fow/Cpm*np.dot(K,pv) ) +0.5*(Fow/Cpm*np.dot(K,pvp))
-	# unite array
-	return np.hstack([T_term,pv_term])
-
-
-def fc_coupled_HAM2(vec_Tpvp,vec_Tpv,K,Fo,Fow,dt,rho,Cp,Cpm,Lv):
-
-	# split array 
-	n=int(len(vec_Tpv)/2) # number of size classes
-	T,pv   = vec_Tpv[0:n] ,vec_Tpv[n:]
-	Tp,pvp = vec_Tpvp[0:n],vec_Tpvp[n:]
-	
-	# prepare updating the props
+	# we need phi in order to actualise w
 	phi=pv/fc_pvsat(T)*100
-	phi[phi>100] = 99.5
-	phi[phi<1] = 0.5
-	# compute water content
+	# compute w 
 	w=fc_w_phi(phi)
-	# update the properties
+	# update the properties 
 	Cpm,dw,dphi=update_Cpm(w,phi,T)
 	Fo=update_Fo(w,k,rho,Cp,dt,dx)
 	Fow=update_Fow(w,dt,dx)
 	
-	rcp=rho*Cp
 	# Crank-Nicolson scheme
-	T_term  = -Tp  +T   +0.5*(Fo*np.dot(K,T) + Lv/rcp*Fow*np.dot(K,pv) ) +0.5*(Fo*np.dot(K,Tp)+Lv/rcp*Fow*np.dot(K,pvp))
-	pv_term = -pvp + pv +0.5*(Fow/Cpm*np.dot(K,pv) ) +0.5*(Fow/Cpm*np.dot(K,pvp))
-	# unite array
+	# explicit and implicit parts for T
+	exp_T=0.5 * (Fo * np.dot(K,T) + Lv * Fow/(rho * Cp) * np.dot(K,pv))
+	imp_T=0.5 * (Fo * np.dot(K,Tp) + Lv *Fow/(rho * Cp) * np.dot(K,pvp))
+	T_term = -Tp + T + exp_T + imp_T
+	# explicit and implicit parts for pv
+	exp_pv=0.5 * (Fow/Cpm * np.dot(K,pv))
+	imp_pv=0.5 * (Fow/Cpm * np.dot(K,pvp))
+	pv_term = -pvp + pv + exp_pv + imp_pv
+	# send back as one array
 	return np.hstack([T_term,pv_term])
 
 # domain size
@@ -119,8 +110,17 @@ K=np.eye(n,n,k=-1)*1 + np.eye(n,n)*-2 + np.eye(n,n,k=1)*1
 K[0,0],K[0,1],K[-1,-1],K[-1,-2]=0,0,0,0
 
 #####################
+# 
 # TIME  TIME TIME TIME TIME
-dt=100
+#
+t=0
+dt=60
+period=24 # pour la sinusoide
+period_sec=period*3600
+nb_period=0.5 # 365
+sim_time=nb_period*24*3600 # seconds
+modulo_storage=int(0.1*3600) #sim_time/dt/100
+
 ##################
 
 # physical props
@@ -134,10 +134,11 @@ Fo=alpha*dt/dx**2
 
 # boundary conditions
 Tleft=10
-Tright=20
 pvleft=0.8*fc_pvsat(Tleft)
-pvright=0.5*fc_pvsat(Tright)
 phileft=pvleft/(fc_pvsat(Tleft))*100
+
+Tright=Tleft
+pvright=pvleft
 phiright=pvright/(fc_pvsat(Tright))*100
 
 ## vectors
@@ -147,56 +148,51 @@ w=np.ones(n)
 T=np.ones(n)
 
 # initial conditions
-pv=np.ones(n)*(pvleft+pvright)/2
-T=np.ones(n)*(Tleft+Tright)/2
-
-Cp_test=fc_Cp_w(w,rho,Cp)
+pv=np.ones(n)*pvleft*0.95
+T=np.ones(n)*Tleft*0.95
 
 phi=pv/fc_pvsat(T)*100
 w=fc_w_phi(phi)
 Cpm,dw,dphi=update_Cpm(w,phi,T)
 Fo=update_Fo(w,k,rho,Cp,dt,dx)
 Fow=update_Fow(w,dt,dx)
-
 #-------------------------------------------------
 # 	Preparing the boundary conditions
 #
 # load pressure and temperature
 pvleft_epw=np.load("pv_out.npy")
 Tleft_epw=np.load("Ta_out.npy")
-# number of seconds
+
+pvleft_epw=pvleft_epw[0:int(24*(nb_period+1))]
+Tleft_epw=Tleft_epw[0:int(24*(nb_period+1))]
+
+#number of seconds
 t_epw=np.arange(0,len(pvleft_epw))*3600
 # resample pv
 x,y = t_epw,pvleft_epw
 y_interp = scipy.interpolate.interp1d(x, y)
 t_new= np.arange(0,t_epw[-1], dt)
 pvleft=y_interp(t_new)
-# resample temperature
+#resample temperature
 x,y = t_epw,Tleft_epw
 y_interp = scipy.interpolate.interp1d(x, y)
 Tleft=y_interp(t_new)
 
 store_Text,store_pvext=[],[]
 store_w,store_phi,store_T,store_pv=[],[],[],[]
-t=0
-period=24 # pour la sinusoide
-period_sec=period*3600
-nb_period=50
-sim_time=nb_period*24*3600 # seconds
-modulo_storage=3600 #sim_time/dt/100
 
 i=0
 # time loop
 pbar = tqdm(total=sim_time)
-while t < sim_time:
+while t <= sim_time:
 	# update boundary conditions
 	T[0]  = Tleft[i]
-	T[n-1]= Tright + 1.5*np.sin(t*2*np.pi/period_sec)
+	T[n-1]= Tright # + 1.5*np.sin(t*2*np.pi/period_sec)
 	pv[0] = pvleft[i]
-	pv[n-1]=pvright + 100*np.sin(t*2*np.pi/period_sec)
+	pv[n-1]=pvright #+ 100*np.cos(t*2*np.pi/period_sec)
 
 	# solve the coupled, non-linear system
-	result_array=fsolve(fc_coupled_HAM2,
+	result_array=fsolve(fc_coupled_HAM,
 			 np.hstack([T,pv]),
 			 args=(np.hstack([T,pv]), K, Fo, Fow, dt, rho, Cp, Cpm, Lv))
 	
@@ -205,15 +201,12 @@ while t < sim_time:
 	
 	# compute phi
 	phi=pv/fc_pvsat(T)*100
-	phi[phi>100] = 99.5
-	phi[phi<1] = 0.5
 	# compute water content
 	w=fc_w_phi(phi)
 	# update the properties
 	Cpm,dw,dphi=update_Cpm(w,phi,T)
 	Fo=update_Fo(w,k,rho,Cp,dt,dx)
 	Fow=update_Fow(w,dt,dx)
-  
 	# update variables
 	pv = pv_plus
 	T=T_plus
@@ -224,3 +217,4 @@ pbar.close()
 
 elapsed_time= (time.time() - start_time) # in seconds
 print('computational effort ',round(elapsed_time/(sim_time/3600),2), ' [seconds/hour simulation]')
+print('elapsed time :', round(elapsed_time/3600), 'h', round((elapsed_time % 3600)/60), 'min')
